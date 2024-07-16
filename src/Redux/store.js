283e1d6c3ff3144941074e043a5f2451c4129d4a
Tulsi Khatri <tulsiyuvasoft447@gmail.com>
2024-07-16 15:46:08 +0530

    crud with redux toolkit and redux persist

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,11 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
-import crudReducer from "./CRUD/crudSlice"
+import crudReducer from "./CRUD/crudSlice";
+import storage from "redux-persist/lib/storage";
+import { persistReducer } from "redux-persist";   
+import { combineReducers } from "@reduxjs/toolkit";
 
-const store = configureStore ({
-
-    reducer:{
-        crud: crudReducer
-    }
+const reducers = combineReducers({
+    crud: crudReducer
 })
 
+const persistConfig = {
+        key : "root",
+        storage
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = configureStore ({
+    reducer : persistedReducer,
+})
+
+// export const persistor = persistStore(store)
 export default store;
--- src/index.js
+++ src/index.js
@@ -6,14 +6,20 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
+import {persistStore} from 'redux-persist'; 
+import { PersistGate } from 'redux-persist/integration/react';
+
+let persistor = persistStore(store)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Provider store={store}>
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
+    </PersistGate>
     </Provider>
   </React.StrictMode>
 );
